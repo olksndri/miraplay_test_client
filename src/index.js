ada8e33ced3afb6cf877c3aede2bfa99034e21bf
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-11-14 16:11:56 +0200

    added basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { App } from "./App.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="https://olksndri.github.io/miraplay_test_client">
       <App />
     </BrowserRouter>
   </React.StrictMode>
